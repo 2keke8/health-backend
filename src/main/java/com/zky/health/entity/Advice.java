28cfac3afd9f652b6e65696ab8204bdb29656cdf
rotchens <2861531823@qq.com>
2022-06-23 16:36:05 +0800

    '插件生成代码'

---
+++ src/main/java/com/zky/health/entity/Advice.java
@@ -0,0 +1,55 @@
+package com.zky.health.entity;
+
+import java.util.Date;
+
+public class Advice {
+    private Integer adviceId;
+
+    private Integer userId;
+
+    private Integer replyId;
+
+    private Date date;
+
+    private String content;
+
+    public Integer getAdviceId() {
+        return adviceId;
+    }
+
+    public void setAdviceId(Integer adviceId) {
+        this.adviceId = adviceId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getReplyId() {
+        return replyId;
+    }
+
+    public void setReplyId(Integer replyId) {
+        this.replyId = replyId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
