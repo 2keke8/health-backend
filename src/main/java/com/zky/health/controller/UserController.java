6b49dc9e6a64e164b0401d9e18c2ae82ea7c9d2a
赵科宇 <2540560264@qq.com>
2022-06-24 11:37:43 +0800

    登录功能

--- src/main/java/com/zky/health/controller/UserController.java
+++ src/main/java/com/zky/health/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.zky.health.controller;
 
-import com.zky.health.dao.UserMapper;
+import com.zky.health.entity.Result;
 import com.zky.health.entity.User;
+import com.zky.health.service.UserService;
+import com.zky.health.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * @Description:
@@ -19,12 +24,35 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @Autowired
-    UserMapper userMapper;
+    UserService userService;
 
-    @RequestMapping("/queryAll")
-    public User queryAll(){
-        User user = userMapper.selectByPrimaryKey(1);
-        return user;
+    @RequestMapping("/login")
+    public Result login(String username, String password){
+
+        if(!StringUtils.hasText(username) || !StringUtils.hasText(password)){
+            return new Result("401","用户名或密码不能为空哦~");
+        }
+
+        int res = userService.login(username, password);
+
+        if(res == 0){
+            return new Result("401","用户名不存在，请联系管理员~");
+        }
+        //判断密码是否正确
+        if(res == -1){
+            return new Result("401","密码错误，请输入正确密码！");
+        }
+
+        //封装返回结果
+        HashMap<String, Object> data = new HashMap<>();
+
+        User user = userService.selectUserByname(username);
+        String token = userService.createToken(username);
+        data.put("user",user);
+        data.put("token",token);
+        //登录成功
+        return new Result("200","登录成功，欢迎您~", data);
+
     }
 
 }
--- src/main/java/com/zky/health/entity/Result.java
+++ src/main/java/com/zky/health/entity/Result.java
@@ -1,4 +1,4 @@
-package com.zky.health.controller.entity;
+package com.zky.health.entity;
 
 /**
  * @Description:
@@ -16,8 +16,31 @@ import java.io.Serializable;
  */
 public class Result implements Serializable{
     private boolean flag;//执行结果，true为执行成功 false为执行失败
+
+    private String code;//返回状态码
     private String message;//返回结果信息
     private Object data;//返回数据
+
+
+    public Result(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Result(String code, String message, Object data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
     public Result(boolean flag, String message) {
         super();
         this.flag = flag;
