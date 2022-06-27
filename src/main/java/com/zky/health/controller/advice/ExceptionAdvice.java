57e59e657b541a37b736798f04e179972363124e
赵科宇 <2540560264@qq.com>
2022-06-27 18:12:51 +0800

    修改预约设置bug

--- src/main/java/com/zky/health/controller/advice/ExceptionAdvice.java
+++ src/main/java/com/zky/health/controller/advice/ExceptionAdvice.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 /**
  * @Description: 异常通知类
